7d1932a3b2c29081475b90f6852b9bc19ecd1621
hungnqbsaf200017@fpt.edu.vn <hungnqbsaf200017@fpt.edu.vn>
2022-04-19 15:36:12 +0700

    push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    private $em;
+    private $categoryRepository;
+    private $productRepository;
+
+    public function __construct(CategoryRepository $categoryRepository, ProductRepository $productRepository, EntityManagerInterface $em)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->productRepository = $productRepository;
+        $this->em = $em;
+    }
+
+
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
@@ -27,8 +43,18 @@ class HomeController extends AbstractController
     #[Route('/list/product', name: 'app_list_product')]
     public function productAction(): Response
     {
-        return $this->render('home/homePage.html.twig', [
-            'controller_name' => 'HomeController',
+        $product = $this->productRepository->findAll();
+        return $this->render('home/listProduct.html.twig', [
+            'product_list' => $product,
+        ]);
+    }
+
+    #[Route('/details/product/{id}', name: 'app_details_product')]
+    public function detailsAction($id): Response
+    {
+        $getOne = $this -> productRepository -> find($id);
+        return $this->render('home/detailProduct.html.twig', [
+            'details' => $getOne,
         ]);
     }
 
